1ff218ce6e4596d6c3357779c101db0b17ace48f
Luke Cann <luke@lc-apps.co.uk>
2022-09-04 18:17:58 +0100

    Improve build pipeline

--- tools/pack/PackMain.hx
+++ tools/pack/PackMain.hx
@@ -3,10 +3,13 @@ package;
 import haxe.Template;
 import sys.FileSystem;
 import sys.io.File;
+import sys.io.Process;
 
 class PackMain {
 	private static inline var indexSrc:String = "res/index.html";
 	private static inline var scriptSrc:String = "build/death.js";
+	private static inline var scriptMin:String = "build/death-min.js";
+	private static inline var scriptRoll:String = "build/death-roll.js";
 	private static inline var zzfxSrc:String = "res/ZzFXMicro.min.js";
 
 	private static inline var finalDir:String = "build/final/";
@@ -17,11 +20,16 @@ class PackMain {
 	private static var lastSize:Int = -1;
 
 	public static function main() {
-		lastSize = FileSystem.stat(packageFile).size;
+		if (FileSystem.exists(packageFile)) {
+			lastSize = FileSystem.stat(packageFile).size;
+		}
 
 		clean();
-		build();
 		minify();
+		build(scriptSrc, "index.html");
+		build(scriptMin, "index-m.html");
+		build(scriptRoll, "index-r.html");
+		minifyHtml("index-m.html");
 		pack();
 	}
 
@@ -42,18 +50,23 @@ class PackMain {
 		cleanDir(finalDir);
 	}
 
-	public static function build() {
+	public static function minify() {
+		Sys.command("uglifyjs", ["-o", scriptMin, scriptSrc]);
+		// Sys.command("npx", ["roadroller", scriptSrc, "-o", scriptRoll]);
+	}
+
+	public static function build(scriptFile, outputFile) {
 		var pageTemplate:String = File.getContent(indexSrc);
-		var script:String = File.getContent(scriptSrc);
+		var script:String = File.getContent(scriptFile);
 		var zzfx:String = File.getContent(zzfxSrc);
 
 		var tpl:Template = new Template(pageTemplate);
 		var out:String = tpl.execute({src: script, zzfx: zzfx});
 
-		File.saveContent(finalUncompressedDir + "index.html", out);
+		File.saveContent(finalUncompressedDir + outputFile, out);
 	}
 
-	public static function minify() {
+	public static function minifyHtml(buildFile) {
 		Sys.command("html-minifier", [
 			"--collaspse-boolean-attributes",
 			"--collapse-inline-tag-whitespace",
@@ -70,7 +83,7 @@ class PackMain {
 			"--use-short-doctype",
 			"-o",
 			finalMinifiedDir + "index.html",
-			finalUncompressedDir + "index.html"
+			finalUncompressedDir + buildFile
 		]);
 	}
 
